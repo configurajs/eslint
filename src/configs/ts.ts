c720d032ca1ad010c587a4df4727a6d0943a83d6
haoziqaq <357229046@qq.com>
2025-01-07 23:38:25 +0800

    style: cleanup

--- src/configs/ts.ts
+++ src/configs/ts.ts
@@ -12,8 +12,6 @@ export function createTsConfig(options: CreateTsConfigOptions = {}): Linter.Conf
 
   const files = ['**/*.?([cm])ts', '**/*.?([cm])tsx', ...exts.map((ext) => `**/*.${ext}`)]
 
-  console.log(tsPlugin.configs['eslint-recommended'].overrides![0].rules)
-
   return [
     {
       name: createConfigName('ts/setup'),
