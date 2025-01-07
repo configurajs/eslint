079cfddc818d07f6e3b38d30daea4d9da3228f80
haoziqaq <357229046@qq.com>
2025-01-07 23:25:55 +0800

    feat(ts): support type aware and update overrides recommended rules

--- src/configs/ts.ts
+++ src/configs/ts.ts
@@ -12,6 +12,8 @@ export function createTsConfig(options: CreateTsConfigOptions = {}): Linter.Conf
 
   const files = ['**/*.?([cm])ts', '**/*.?([cm])tsx', ...exts.map((ext) => `**/*.${ext}`)]
 
+  console.log(tsPlugin.configs['eslint-recommended'].overrides![0].rules)
+
   return [
     {
       name: createConfigName('ts/setup'),
@@ -27,6 +29,10 @@ export function createTsConfig(options: CreateTsConfigOptions = {}): Linter.Conf
         parserOptions: {
           sourceType: 'module',
           extraFileExtensions: exts.map((ext) => `.${ext}`),
+          tsconfigRootDir: process.cwd(),
+          projectService: {
+            allowDefaultProject: ['./*.js'],
+          },
         },
       },
     },
@@ -34,9 +40,28 @@ export function createTsConfig(options: CreateTsConfigOptions = {}): Linter.Conf
       name: createConfigName('ts/rules'),
       files,
       rules: {
-        'no-array-constructor': 'off',
-        'no-unused-vars': 'off',
-        'no-unused-expressions': 'off',
+        'constructor-super': 'off',
+        'getter-return': 'off',
+        'no-class-assign': 'off',
+        'no-const-assign': 'off',
+        'no-dupe-args': 'off',
+        'no-dupe-class-members': 'off',
+        'no-dupe-keys': 'off',
+        'no-func-assign': 'off',
+        'no-import-assign': 'off',
+        'no-new-symbol': 'off',
+        'no-new-native-nonconstructor': 'off',
+        'no-obj-calls': 'off',
+        'no-redeclare': 'off',
+        'no-setter-return': 'off',
+        'no-this-before-super': 'off',
+        'no-undef': 'off',
+        'no-unreachable': 'off',
+        'no-unsafe-negation': 'off',
+        'no-var': 'error',
+        'prefer-const': 'error',
+        'prefer-rest-params': 'error',
+        'prefer-spread': 'error',
         '@typescript-eslint/no-array-constructor': 'error',
         '@typescript-eslint/no-duplicate-enum-values': 'error',
         '@typescript-eslint/no-extra-non-null-assertion': 'error',
@@ -48,7 +73,6 @@ export function createTsConfig(options: CreateTsConfigOptions = {}): Linter.Conf
         '@typescript-eslint/no-unnecessary-type-constraint': 'error',
         '@typescript-eslint/no-unsafe-declaration-merging': 'error',
         '@typescript-eslint/no-unsafe-function-type': 'error',
-        '@typescript-eslint/no-unused-expressions': 'error',
         '@typescript-eslint/no-unused-vars': 'error',
         '@typescript-eslint/no-wrapper-object-types': 'error',
         '@typescript-eslint/prefer-as-const': 'error',
@@ -58,6 +82,14 @@ export function createTsConfig(options: CreateTsConfigOptions = {}): Linter.Conf
         '@typescript-eslint/no-explicit-any': 'off',
         '@typescript-eslint/no-for-in-array': 'off',
         '@typescript-eslint/no-empty-object-type': 'off',
+        '@typescript-eslint/no-unused-expressions': [
+          'error',
+          {
+            allowShortCircuit: true,
+            allowTaggedTemplates: true,
+            allowTernary: true,
+          },
+        ],
       },
     },
   ]
